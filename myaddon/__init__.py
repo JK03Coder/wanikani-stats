370e14672f7c837f64166927c13eed2043832c13
justin <jnkempton@gmail.com>
2024-01-24 00:16:09 +1300

    start date stat

--- myaddon/__init__.py
+++ myaddon/__init__.py
@@ -1,3 +1,6 @@
+from math import floor
+from datetime import datetime
+
 from aqt import mw
 from aqt.utils import showInfo
 from aqt.qt import *
@@ -51,8 +54,13 @@ else:
 
 
 def fetch_stats_op(col: Collection) -> dict:
-    stats_dict = {"user_level": get_user_level(col),
-                  "radicals_learned": "6969"}
+    stats_dict = {
+        "user_level": get_user_level(col),
+        "radicals_learned": get_radicals_learned(col),
+        "kanji_learned": get_kanji_learned(col),
+        "vocabulary_learned": get_vocabulary_learned(col),
+        "start_date": get_start_date(col),
+    }
 
     return stats_dict
 
@@ -86,3 +94,55 @@ def get_user_level(col: Collection) -> int:
 
     # Return None if no relevant tag is found
     return None
+
+
+def get_radicals_learned(col: Collection) -> int:
+    deck_name = "Wanikani Ultimate 3: Tokyo Drift"
+
+    query = f'deck:"{deck_name}" -is:new ("tag:radical" or "Card_Type:Radical")'
+    learned_radical_card_ids = col.find_cards(query)
+
+    return len(learned_radical_card_ids)
+
+
+def get_kanji_learned(col: Collection) -> int:
+    deck_name = "Wanikani Ultimate 3: Tokyo Drift"
+
+    query = f'deck:"{deck_name}" -is:new ("tag:kanji" or "Card_Type:Kanji")'
+    learned_kanji_card_ids = col.find_cards(query)
+
+    return floor(len(learned_kanji_card_ids)/2)
+
+
+def get_vocabulary_learned(col: Collection) -> int:
+    deck_name = "Wanikani Ultimate 3: Tokyo Drift"
+
+    query = f'deck:"{deck_name}" -is:new ("tag:vocabulary" or "Card_Type:Vocabulary")'
+    learned_vocabulary_card_ids = col.find_cards(query)
+
+    return floor(len(learned_vocabulary_card_ids)/2)
+
+
+def get_start_date(col: Collection) -> str:
+    deck_name = "Wanikani Ultimate 3: Tokyo Drift"
+
+    query = """
+        select min(id) from revlog 
+        where cid in (
+            select id from cards where did = (
+                select id from decks where name = ?
+            )
+        )
+    """
+    first_review_id = col.db.scalar(query, deck_name)
+
+    if first_review_id and first_review_id > 0:
+        # Convert the timestamp from milliseconds to seconds
+        first_review_date = datetime.utcfromtimestamp(first_review_id / 1000)
+        date_str = first_review_date.strftime('%Y-%m-%d')
+
+        # Calculate days ago
+        days_ago = (datetime.utcnow() - first_review_date).days
+        return f"{date_str} ({days_ago} days ago)"
+    else:
+        return "---"
